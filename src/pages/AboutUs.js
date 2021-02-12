e4106f66f1ee21bf5bfe458240998ddd7f8c6fe4
Yacheng Li <yacheng3249@gmail.com>
2021-02-12 14:27:41 +0800

    3.Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import AboutSection from '../components/AboutSection'
+import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 
 const AboutUS = () => {
   return (
-    <AboutSection />
+    <>
+      <AboutSection />
+      <ServicesSection />
+    </>
   );
 }
 
