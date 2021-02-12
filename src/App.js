a14b8b072af9a8cf2d5337334ffd14d31c5cef16
Yacheng Li <yacheng3249@gmail.com>
2021-02-13 01:21:58 +0800

    8. Creating Nav

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import GlobalStyle from "./components/GlobalStyle";
 //Import Pages
 import AboutUS from './pages/AboutUs';
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUS />
     </div>
   );
