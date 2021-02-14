20e1fa9194b1eadd5e21dadeb28922739bcad1c6
Yacheng Li <yacheng3249@gmail.com>
2021-02-14 21:42:38 +0800

    9. React Router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import React from 'react';
 import GlobalStyle from "./components/GlobalStyle";
 //Import Pages
 import AboutUS from './pages/AboutUs';
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
 import Nav from "./components/Nav";
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUS />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUS />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
