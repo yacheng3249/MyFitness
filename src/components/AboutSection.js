3240fd8e1b70820a8a423c314598f7be155b6cee
Yacheng Li <yacheng3249@gmail.com>
2021-02-18 00:09:04 +0800

    16. Our Work Animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -30,7 +30,7 @@ const AboutSection = () => {
         <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
+        <motion.img variants={photoAnim} initial="hidden" animate="show" src={home1} alt="guy with a camera" />
       </Image>
       <Wave />
     </About>
