d5fe72cf1e5ecffad94e18e3b2b6fb2e4e52376a
Yacheng Li <yacheng3249@gmail.com>
2021-02-17 21:12:50 +0800

    15. Wave Animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -5,6 +5,7 @@ import { About, Description, Image, Hide } from "../styles";
 //Framer MOtion
 import { motion } from "framer-motion";
 import { titleAnim, fade, photoAnim } from "../animation";
+import Wave from "./Wave";
 
 const AboutSection = () => {
   return (
@@ -31,6 +32,7 @@ const AboutSection = () => {
       <Image>
         <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
       </Image>
+      <Wave />
     </About>
   );
 }
