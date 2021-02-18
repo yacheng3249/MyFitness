f9f9c3186a7b88453b4ba8748bce11a4e246915d
Yacheng Li <yacheng3249@gmail.com>
2021-02-18 16:01:40 +0800

    17. Reusable Components

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import styled from "styled-components";
 import { About } from "../styles";
+import Toggle from "./Toggle";
 
 const FaqSection = () => {
+  
   return (
     <Faq>
       <h2>Any Questions <span>FAQ</span></h2>
-      <div className="question">
-        <h4>How Do I Start?</h4>
-        <div className="answer">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          </p>
+      <Toggle>
+        <div className="question">
+          <h4>How Do I Start?</h4>
+          <div className="answer">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            </p>       
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
+      </Toggle>
       <div className="question">
         <h4>Daily Schedule</h4>
         <div className="answer">
